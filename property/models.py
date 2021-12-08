b0ca4d759867123e2ac565c7c4c3aff36dc08ebc
Anna <44573387+annfike@users.noreply.github.com>
2021-12-08 20:35:44 +0300

    Add link Flat-Owner

--- property/models.py
+++ property/models.py
@@ -5,8 +5,6 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -54,12 +52,6 @@ class Flat(models.Model):
         related_name='liked_flats',
         verbose_name='Кто лайкнул',
         blank=True,
-    )
-    owner_pure_phone = PhoneNumberField(
-        'Нормализованный номер владельца',
-        null=True,
-        blank=True,
-        db_index=True
     )
 
     def __str__(self):
