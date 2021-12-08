89c7ad99e4c8307e9e66008475f97c14c13d5d46
Anna <44573387+annfike@users.noreply.github.com>
2021-12-08 19:25:40 +0300

    Add model Owner

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat, Complaint
+from .models import Flat, Complaint, Owner
 
 
 @admin.register(Flat)
@@ -16,10 +16,18 @@ class FlatAdmin(admin.ModelAdmin):
     )
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
+    raw_id_fields = ('liked_by',)
 
 
 @admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     list_display = ('user', 'flat', 'text',)
     list_editable = ('flat', 'text',)
+    raw_id_fields = ('flat',)
+
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'owners_phonenumber', 'owner_pure_phone',)
+    list_editable = ('owners_phonenumber', 'owner_pure_phone',)
     raw_id_fields = ('flat',)
--- property/models.py
+++ property/models.py
@@ -84,3 +84,31 @@ class Complaint(models.Model):
 
     def __str__(self):
         return f'Жалоба от {self.user}'
+
+
+class Owner(models.Model):
+    flat = models.ManyToManyField(
+        Flat,
+        verbose_name='Квартиры в собственности',
+        related_name='flats',
+        db_index=True,
+    )
+    owner = models.CharField(
+        'ФИО владельца', 
+        max_length=200, 
+        db_index=True
+    )
+    owners_phonenumber = models.CharField(
+        'Номер владельца',
+        max_length=20,
+        db_index=True
+    )
+    owner_pure_phone = PhoneNumberField(
+        'Нормализованный номер владельца',
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    def __str__(self):
+        return f'Владелец {self.owner}'
